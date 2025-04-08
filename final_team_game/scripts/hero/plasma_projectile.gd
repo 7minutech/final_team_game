30f58047c52ba48f46deb1e9d892601d89c75b77
JordanU-GH <junderwood9@stu.jsu.edu>
2025-04-08 17:11:05 -0500

    Added comments/whitespace for clarity in plasma_projectile.gd

--- final_team_game/scripts/hero/plasma_projectile.gd
+++ final_team_game/scripts/hero/plasma_projectile.gd
@@ -6,6 +6,7 @@ extends RigidBody2D
 # Variables for stats
 var speed: int
 var damage: int
+
 var direction: Vector2
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +15,7 @@ func _ready() -> void:
 	max_contacts_reported = 300
 	self.position = get_tree().current_scene.find_child("Hero").position
 
+# Called every physics process frame
 func _physics_process(delta: float) -> void:
 	self.move_and_collide((direction * delta).normalized() * speed)
 	
@@ -26,14 +28,16 @@ func setDirection(d: Vector2) -> void:
 # Function for adjusting speed
 func setSpeed(spd: int) -> void:
 	speed = spd
+##
 # Function for adjusting damage
 func setDamage(dmg: int) -> void:
 	damage = dmg
 
+# Function to despawn the projectile if it does not collide
 func _on_despawn_timeout() -> void:
 	self.queue_free()
 
-
+# Function to determine what to do if a body is hit
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("enemies"):
 		self.queue_free()
