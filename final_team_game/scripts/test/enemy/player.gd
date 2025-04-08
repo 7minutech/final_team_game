f155e107bb86f78a5376fb4dc7c6d4b21ee6b8d7
7minutech <elijohnson1107@gmail.com>
2025-04-07 19:06:05 -0500

    feat: added testing for enemy logic

---
+++ final_team_game/scripts/test/enemy/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+@export var SPEED: float = 300.0
+
+func _ready() -> void:
+	PlayerObserver.player = self
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2i
+	if Input.is_action_pressed("move_up"):
+		direction = Vector2i.UP
+	if Input.is_action_pressed("move_down"):
+		direction = Vector2i.DOWN
+	if Input.is_action_pressed("move_right"):
+		direction = Vector2i.RIGHT
+	if Input.is_action_pressed("move_left"):
+		direction = Vector2i.LEFT
+	velocity = direction * SPEED
+
+	move_and_slide()
