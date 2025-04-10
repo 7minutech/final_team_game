5db3f5c8a721f8bbceea3570a597ea58b7b4b562
7minutech <elijohnson1107@gmail.com>
2025-04-10 10:04:07 -0500

    feat: enemy drop xp orb on death

--- final_team_game/scripts/enemy/enemy_1.gd
+++ final_team_game/scripts/enemy/enemy_1.gd
@@ -23,6 +23,7 @@ func _physics_process(_delta: float) -> void:
 		velocity = direction.normalized() * speed
 		
 		if health <= 0:
+			drop_xp_orb()
 			await get_tree().create_timer(0.15).timeout
 			self.queue_free()
 		move_and_slide()
@@ -80,7 +81,11 @@ func set_pitch_scale() -> void:
 	var pitch := randf_range(1.2,1.5)
 	$LostHeatlhSound.pitch_scale = pitch
 
-
+func drop_xp_orb() -> void:
+	var xp_orb: PackedScene = preload("res://scenes/item/xp_orb.tscn")
+	var xp_orb_instance: XPOrb = xp_orb.instantiate()	
+	xp_orb_instance.position = self.position
+	get_parent().add_child(xp_orb_instance)
 	
 	
 
