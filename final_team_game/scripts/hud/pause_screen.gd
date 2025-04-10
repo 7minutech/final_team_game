cebaaa08b6053ab2ee7a97cfccd360fd8f5da839
JordanU-GH <junderwood9@stu.jsu.edu>
2025-04-10 17:25:06 -0500

    Added place for background images for abilities on pause screen

--- final_team_game/scripts/hud/pause_screen.gd
+++ final_team_game/scripts/hud/pause_screen.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const testImage = preload("res://icon.svg")
+const image1 = preload("res://icon.svg")
 const image2 = preload("res://assets/hud/ability_symbols/filled_pip/AbilityPip_Filled.png")
 const image3 = preload("res://assets/hud/ability_symbols/empty_pip/AbilityPip_Empty.png")
 const message: String = "Pretyped Text"
@@ -8,7 +8,6 @@ const message: String = "Pretyped Text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	addAbility(testImage, str(1))
 	addAbility(image2, str(2))
 	addAbility(image3, str(3))
 
@@ -20,5 +19,6 @@ func _process(delta: float) -> void:
 func addAbility(image, tooltip: String) -> void:
 	for i in range(4):
 		$Ability.add_image(image, 50, 50, Color(1,1,1), INLINE_ALIGNMENT_CENTER, Rect2(0,0,0, 0), null, false, tooltip, false)
-
+		$AbilityBG.add_image(image1, 50, 50, Color(1,1,1), INLINE_ALIGNMENT_CENTER)
+	
 	
