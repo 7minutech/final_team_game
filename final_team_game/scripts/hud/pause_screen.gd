8ac381af08221fb4dda4b5bd0eb1e96536ca874c
JordanU-GH <junderwood9@stu.jsu.edu>
2025-04-10 17:20:13 -0500

    Added functionality for the ability sprites to be added into the game along with their descriptions onto the pause screen.

---
+++ final_team_game/scripts/hud/pause_screen.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+const testImage = preload("res://icon.svg")
+const image2 = preload("res://assets/hud/ability_symbols/filled_pip/AbilityPip_Filled.png")
+const image3 = preload("res://assets/hud/ability_symbols/empty_pip/AbilityPip_Empty.png")
+const message: String = "Pretyped Text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	addAbility(testImage, str(1))
+	addAbility(image2, str(2))
+	addAbility(image3, str(3))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.f
+func _process(delta: float) -> void:
+	pass
+
+# Function to add ability images to the label
+func addAbility(image, tooltip: String) -> void:
+	for i in range(4):
+		$Ability.add_image(image, 50, 50, Color(1,1,1), INLINE_ALIGNMENT_CENTER, Rect2(0,0,0, 0), null, false, tooltip, false)
+
+	
