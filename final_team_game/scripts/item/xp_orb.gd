531ac8280d05f85374615c25ebd8a20af78e80fd
7minutech <elijohnson1107@gmail.com>
2025-04-10 17:53:45 -0500

    feat: added pick up sound for orb

--- final_team_game/scripts/item/xp_orb.gd
+++ final_team_game/scripts/item/xp_orb.gd
@@ -18,13 +18,18 @@ func set_xp_value(xp_value: int) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	var parent = area.get_parent()
-	if parent is Player:  # or use a type check if possible
+	if parent is Player:
+		set_pitch_scale()
+		$PickUpSound.play()
 		var player: Player = parent
 		var tween = create_tween()
 		tween.tween_property(self, "global_position", player.global_position, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 		tween.connect("finished", Callable(self, "_on_tween_finished").bind(player))
-	pass # Replace with function body.
 
 func _on_tween_finished(player: Player) -> void:
 	player.give_xp(self)
 	queue_free()
+
+func set_pitch_scale() -> void:
+	var pitch := randf_range(0.9,1.1)
+	$PickUpSound.pitch_scale = pitch
