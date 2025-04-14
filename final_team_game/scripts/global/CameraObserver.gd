3afc459c5e4054bc4c6898e57f314c25d2cc0572
JordanU-GH <junderwood9@stu.jsu.edu>
2025-04-13 21:53:08 -0500

    fix: Adjusted drone sprite sheet to remove random lines at the bottom. fix: resolved a warning that appeared in the debugger

--- final_team_game/scripts/global/CameraObserver.gd
+++ final_team_game/scripts/global/CameraObserver.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	camera_positions = get_camera_edges(player_camera)
 
 func get_camera_edges(camera: Camera2D) -> Dictionary:
