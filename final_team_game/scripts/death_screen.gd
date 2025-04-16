fad3a6ee64f2883eacbd5d9f715668154305bdfb
ayee-d <bigdt8616@gmail.com>
2025-04-16 10:06:26 -0500

    feat: added sound files and death screen scene

---
+++ final_team_game/scripts/death_screen.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@onready var restart_button = $VBoxContainer/RestartButton
+@onready var quit_button = $VBoxContainer/QuitButton
+@onready var death_sound = $DeathSound
+@onready var hover_sound = $Hover
+@onready var click_sound = $ClickSound
+
+
+
+func _ready():
+	if restart_button:
+		restart_button.pressed.connect(on_restart_pressed)
+		restart_button.mouse_entered.connect(on_hover)
+
+	if quit_button:
+		quit_button.pressed.connect(on_quit_pressed)
+		quit_button.mouse_entered.connect(on_hover)
+
+	if death_sound:
+		death_sound.play()
+
+func on_hover():
+	hover_sound.play()
+
+func on_restart_pressed():
+	click_sound.play()
+	await click_sound.finished
+	get_tree().change_scene_to_file("res://scenes/test/TestEnemyMain2.tscn")
+
+func on_quit_pressed():
+	click_sound.play()
+	await click_sound.finished
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
