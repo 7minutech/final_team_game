8162da14b32684611c311e47f59a1f1c83620e68
7minutech <elijohnson1107@gmail.com>
2025-04-23 10:03:38 -0500

    feat: garlic upgrades and desc

--- final_team_game/scripts/ability/active/garlic.gd
+++ final_team_game/scripts/ability/active/garlic.gd
@@ -2,29 +2,62 @@ extends Sprite2D
 
 class_name Garlic
 
+const ability_desc: Dictionary = {
+	1: "Increase damage by 5",
+	2: "Increase garlic area of effect by 25%",
+	3: "Decrease damage cooldown to 0.75s",
+	4: "Increase garlic area of effect by 50%",
+	5: "Increase damage by 10",
+	6: "Decrease damage cooldown to 0.5s",
+	7: "Increase damage by 15",
+	8: "Increase garlic area of effect by 75%",
+	9: "Increase damage by 20"
+}
+
+
 const INITIAL_SCALE: Vector2 = Vector2(0.2,0.2)
+const INITIAL_DAMAGE: int = 20
+const INITAIAL_TICK_INTERVAL: float = 1.0
 
 var max_qty: float = 6
 var radius: float
 var damage: int = 20
 var damage_targets = []
-var TICK_INTERVAL: float = 1.0
+var tick_interval: float = 1.0
 var tick_timer: float = 0.0
 var garlic_scale = INITIAL_SCALE
 var active = true
 
 func _process(delta: float) -> void:
 	tick_timer += delta
-	if tick_timer >= TICK_INTERVAL:
+	if tick_timer >= tick_interval:
 		tick_timer = 0
 		for target in damage_targets:
 			if is_instance_valid(target) and target.has_method("loseHealth"):
 				target.loseHealth(damage)
 
-func update_stat(qty:float) -> void:
-	# if qty is 2 then multiplier is 1.2
-	var multiplier: float = 1 + (qty / 10)
-	scale = INITIAL_SCALE * multiplier
+func update_stat(qty: int) -> void:
+	match qty:
+		1:
+			set_damage(INITIAL_DAMAGE + 5)
+		2:
+			set_garlic_scale(INITIAL_SCALE * 1.25)
+		3:
+			set_damage_cd(INITAIAL_TICK_INTERVAL - 0.25)
+		4:
+			set_garlic_scale(INITIAL_SCALE * 1.50)
+		5:
+			set_damage(INITIAL_DAMAGE + 10)
+		6:
+			set_damage_cd(INITAIAL_TICK_INTERVAL - 0.5)
+		7:
+			set_damage(INITIAL_DAMAGE + 15)
+		8:
+			set_garlic_scale(INITIAL_SCALE * 1.75)
+		9:
+			set_damage(INITIAL_DAMAGE + 20)
+		_:
+			print("Invalid level")
 
 func _on_damage_ticker_timeout() -> void:
 	for target in damage_targets:	
@@ -38,9 +71,17 @@ func _on_garlic_area_body_entered(body: Node2D) -> void:
 		damage_targets.append(body)
 	pass # Replace with function body.
 
-
 func _on_garlic_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
 		#body.loseHealth(damage)
 		damage_targets.erase(body)
 	pass # Replace with function body.
+
+func set_damage_cd(new_time: float) -> void:
+	tick_interval = new_time
+
+func set_damage(new_damage: int) -> void:
+	damage = new_damage
+
+func set_garlic_scale(new_scale: Vector2) -> void:
+	scale = new_scale
