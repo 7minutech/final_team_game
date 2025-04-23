e9b5ca23aea6c37118295679acceebd4b98badc5
7minutech <elijohnson1107@gmail.com>
2025-04-23 09:04:44 -0500

    feat: orbital beam damages enemies

--- final_team_game/scripts/ability/passive/orbital_beam.gd
+++ final_team_game/scripts/ability/passive/orbital_beam.gd
@@ -5,8 +5,11 @@ const INTIAL_BEAM_SCALE: Vector2 = Vector2(1.162,1.123)
 var radius: int = 125
 var angle: float = 0.0
 var speed: float = 0.5
-var dmg: int = 20 
+var damage: int = 20 
 var beam_scale: Vector2 = INTIAL_BEAM_SCALE
+var enemies_in_beam: Array = []
+var damage_timer := 1.0 
+var time_accumulator := 0.0
 
 func _ready() -> void:
 	$Beam.scale = INTIAL_BEAM_SCALE
@@ -25,6 +28,15 @@ func _process(delta):
 		# Point away from the center 
 		# don't need to rotate b/c it's already looking away
 		look_at(center)
+	time_accumulator += delta
+	if time_accumulator >= damage_timer:
+		# Use a copy to avoid modifying the array 
+		for enemy in enemies_in_beam.duplicate():
+			if enemy == null or not enemy.is_inside_tree():
+				enemies_in_beam.erase(enemy)
+			else:
+				enemy.loseHealth(damage)
+		time_accumulator = 0.0
 
 func update_stat(qty:float) -> void:
 	# if qty is 2 then multiplier is 1.2
@@ -32,3 +44,17 @@ func update_stat(qty:float) -> void:
 	var new_scale: Vector2 = INTIAL_BEAM_SCALE * multiplier
 	beam_scale = new_scale
 	$Beam.scale = beam_scale
+
+
+func _on_beam_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemies") and not enemies_in_beam.has(body):
+		if body != null and body.is_inside_tree():
+			body.loseHealth(damage)
+		enemies_in_beam.append(body)
+	pass # Replace with function body.
+
+
+func _on_beam_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("enemies"):
+		enemies_in_beam.erase(body)
+	pass # Replace with function body.
